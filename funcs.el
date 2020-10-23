0530cbc2349628c4befa8a2d7a499216ffbee10d
Daniel Nicolai <dalanicolai@gmail.com>
2020-10-23 10:07:02 +0200

    Add group switch bindings

--- funcs.el
+++ funcs.el
@@ -1,4 +1,5 @@
 (defun tabs-timer-initialize ()
+  
   (setq tabs-timer (run-with-timer 3 nil (lambda () (centaur-tabs-local-mode 1)))))
 
 (add-hook 'window-setup-hook 'tabs-timer-initialize)
@@ -6,16 +7,32 @@
 (defun centaur-tabs-switch-and-hide (arg)
   (cancel-timer tabs-timer)
   (centaur-tabs-local-mode 1)
-  (if arg
-      (centaur-tabs-backward)
-    (centaur-tabs-forward))
+  ;; (if arg
+  ;;     (centaur-tabs-backward)
+  ;;   (centaur-tabs-forward))
+  (cond ((equal arg 'backward)
+         (centaur-tabs-backward))
+        ((equal arg 'forward)
+         (centaur-tabs-forward))
+        ((equal arg 'backward-group)
+         (centaur-tabs-backward-group))
+        ((equal arg 'forward-group)
+         (centaur-tabs-forward-group)))
   (centaur-tabs-local-mode 0)
   (setq tabs-timer (run-with-timer 2 nil (lambda () (centaur-tabs-local-mode 1)))))
 
 (defun centaur-tabs-forward-and-hide ()
   (interactive)
-  (centaur-tabs-switch-and-hide nil))
+  (centaur-tabs-switch-and-hide 'forward))
 
 (defun centaur-tabs-backward-and-hide ()
   (interactive)
-  (centaur-tabs-switch-and-hide t))
+  (centaur-tabs-switch-and-hide 'backward))
+
+(defun centaur-tabs-forward-group-and-hide ()
+  (interactive)
+  (centaur-tabs-switch-and-hide 'forward-group))
+
+(defun centaur-tabs-backward-group-and-hide ()
+  (interactive)
+  (centaur-tabs-switch-and-hide 'backward-group))
